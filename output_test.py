d527c637add2fdbab8fa57aa74884c985298148a
Klevis <klevis.aliaj@utah.edu>
2020-09-06 11:25:01 -0600

    Outputting a trial summary as well.

--- output_test.py
+++ output_test.py
@@ -7,25 +7,34 @@ from parameters import Params
 from database.dynamic_trial import DynamicTrial
 from graphing.plotters import SmoothingOutputPlotter
 import graphing.graph_utils as graph
-from smoothing.kf_filtering_helpers import kf_filter_marker
+from smoothing.kf_filtering_helpers import kf_filter_marker, InsufficientDataError
 
 
 def trial_plotter(trial, dt, subj_dir):
     trial_dir = subj_dir / trial.trial_name
     trial_dir.mkdir(parents=True, exist_ok=True)
     print('Smoothing trial {}'.format(trial.trial_name))
-    for marker in DynamicTrial.MARKERS:
-        if marker in trial.vicon_data_labeled.columns:
-            marker_pdf = trial_dir / (marker + '.pdf')
-            with PdfPages(marker_pdf) as pdf_file:
-                raw, filled, filtered, smoothed = kf_filter_marker(trial, marker, dt=dt)
+    trial_pdf_file = trial_dir / (trial.trial_name + '.pdf')
+    with PdfPages(trial_pdf_file) as trial_pdf:
+        for marker in DynamicTrial.MARKERS:
+            if marker in trial.vicon_data_labeled.columns:
+                marker_pdf_file = trial_dir / (marker + '.pdf')
+                try:
+                    raw, filled, filtered, smoothed = kf_filter_marker(trial, marker, dt=dt)
+                except InsufficientDataError:
+                    print('Skipping marker {} for trial {} because there is insufficient data to filter.'
+                          .format(marker, trial.trial_name))
+                    continue
                 marker_plotter = SmoothingOutputPlotter(trial.trial_name, marker, raw, filled, filtered, smoothed,
                                                         trial.vicon_endpts)
                 figs = marker_plotter.plot()
-                for fig in figs:
-                    pdf_file.savefig(fig)
-                    fig.clf()
-                    plt.close(fig)
+                with PdfPages(marker_pdf_file) as marker_pdf:
+                    for (fig_num, fig) in enumerate(figs):
+                        marker_pdf.savefig(fig)
+                        if fig_num in [0, 1]:
+                            trial_pdf.savefig(fig)
+                        fig.clf()
+                        plt.close(fig)
 
 
 # ready db
